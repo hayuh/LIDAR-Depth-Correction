0c21eab7238084480144fef79ed49e544f30ef36
hayuh <hyuh@andrew.cmu.edu>
2022-04-18 13:52:17 -0400

    Created csv file for depth_img

--- parse_numpy.py
+++ parse_numpy.py
@@ -1,10 +1,16 @@
 import numpy as np
 from matplotlib import pyplot as plt
+import pandas as pd
 
 
 
-depth_img_array = np.load('depth_image_1649863390457055339.npy') #720 1280-length arrays
-color_img_array = np.load('color_image_1649863390572339047.npy')
+depth_img_array = np.load('depth_image_1650298896740195817.npy') #720 1280-length arrays
+color_img_array = np.load('color_image_1650298896750529763.npy')
+# convert array into dataframe
+DF = pd.DataFrame(depth_img_array)
+  
+# save the dataframe as a csv file
+DF.to_csv("depth_img.csv")
 
 plt.imshow(depth_img_array, cmap='gray')
 plt.show()
